86a433e5fe619542f6c6bc4fffa20f45c4bc69fe
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-22 19:24:49 -0600

    Update form

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const Add = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const contacts = useSelector((state) => state);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const checkEmail = contacts.find(
+      (contact) => contact.email === email && email
+    );
+
+    if (!email || !number || !name) {
+      return toast.warning("Please fill in all fields!");
+    }
+
+    if (checkEmail) {
+      return toast.error("This email already Exists!");
+    }
+  };
+
   return (
     <div className="container">
       <div className="row">
         <h1 className="display-3 text-center">Add Student</h1>
         <div className="col-md-6 shadow mx-auto p-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
-              <input type="text" placeholder="Name" className="form-control" />
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control"
+              />
             </div>
             <div className="form-group">
               <input
                 type="email"
                 placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="form-control"
               />
             </div>
@@ -21,6 +52,8 @@ const Add = () => {
               <input
                 type="number"
                 placeholder="Phone number"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
                 className="form-control"
               />
             </div>
--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -2,11 +2,13 @@ const initialState = [
   {
     id: 0,
     name: "Jonghan Park",
+    email: "rs@gmail.com",
     number: 1234567890,
   },
   {
     id: 1,
     name: "Test Name",
+    email: "test@gmail.com",
     number: 789456123,
   },
 ];
