bfa438b803f32c4c79c95ddd1a67af17d0cdf356
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-23 09:08:52 -0600

    Update update student

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,44 +1,116 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Edit = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
   const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const contacts = useSelector((state) => state);
+  const currentContact = contacts.find(
+    (contact) => contact.id === parseInt(id)
+  );
+
+  useEffect(() => {
+    if (currentContact) {
+      setName(currentContact.name);
+      setEmail(currentContact.email);
+      setNumber(currentContact.number);
+    }
+  }, [currentContact]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const checkEmail = contacts.find(
+      (contact) => contact.id !== parseInt(id) && contact.email === email
+    );
+    const checkNumber = contacts.find(
+      (contact) =>
+        contact.id !== parseInt(id) && contact.number === parseInt(number)
+    );
+
+    if (!email || !number || !name) {
+      return toast.warning("Please fill in all fields!");
+    }
+
+    if (checkEmail) {
+      return toast.error("This email already Exists!");
+    }
+    if (checkNumber) {
+      return toast.error("This number already Exists!");
+    }
+
+    const data = {
+      id: parseInt(id),
+      name,
+      email,
+      number,
+    };
+    dispatch({ type: "UPDATE_CONTACT", payload: data });
+    toast.success("Student updated successfully!");
+    navigate("/");
+  };
+
   return (
     <div className="container">
-      <div className="row">
-        <h1 className="display-3 text-center">Edit Student {id}</h1>
-        <div className="col-md-6 shadow mx-auto p-5">
-          <form>
-            <div className="form-group">
-              <input type="text" placeholder="Name" className="form-control" />
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                placeholder="Email"
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="number"
-                placeholder="Phone number"
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="submit"
-                value="Update Student"
-                className="btn btn-dark"
-              />
-              <Link to="/" className="btn btn-danger ml-3">
-                Cancel
-              </Link>
-            </div>
-          </form>
+      {currentContact ? (
+        <div className="row">
+          <h1 className="display-3 text-center">Edit Student {id}</h1>
+          <div className="col-md-6 shadow mx-auto p-5">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="form-control"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="number"
+                  placeholder="Phone number"
+                  value={number}
+                  onChange={(e) => setNumber(e.target.value)}
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="submit"
+                  value="Update Student"
+                  className="btn btn-dark"
+                />
+                <Link to="/" className="btn btn-danger ml-3">
+                  Cancel
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      ) : (
+        <h1 className="display-3 my-5 text-center">
+          Studentcontact with id {id} not exists
+        </h1>
+      )}
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const contacts = useSelector((state) => state);
   return (
     <div className="container">
       <div className="row">
@@ -11,8 +13,44 @@ const Home = () => {
           </Link>
         </div>
       </div>
-      <div className="col-md-6 mx-auto">
-        <h1>Welcome to react redux contact book</h1>
+      <div className="col-md-10 mx-auto">
+        <table className="table table-hover">
+          <thead className="text-white bg-dark text-center">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Number</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact, id) => {
+              return (
+                <tr key={id}>
+                  <td>{id + 1}</td>
+                  <td>{contact.name}</td>
+                  <td>{contact.email}</td>
+                  <td>{contact.number}</td>
+                  <td>
+                    <Link
+                      to={`/edit/${id}`}
+                      className="btn btn-small btn-primary"
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      to={`/edit/${id}`}
+                      className="btn btn-small btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
