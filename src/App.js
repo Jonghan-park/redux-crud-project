5bc78d097a90946dcddba6692e70f53cfae12d08
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-22 18:06:42 -0600

    Update toastify and navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { ToastContainer } from "react-toastify";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
+      <Navbar />
       <h1>Hello World</h1>
     </div>
   );
