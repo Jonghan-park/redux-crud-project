8c45149891d62e24fc0940d6c1b66f84e8272b77
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-22 18:20:32 -0600

    Update add and edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,6 +9,11 @@ function App() {
     <div className="App">
       <ToastContainer />
       <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/add" element={<Add />} />
+        <Route path="/edit:id" element={<Edit />} />
+      </Routes>
       <h1>Hello World</h1>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>I'm a Nav</div>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2">
+      <Link to="/" className="navbar-brand ml-5">
+        React Redux Contact
+      </Link>
+      <ul className="navbar-nav">
+        <li className="nav-item"></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
